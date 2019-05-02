ca38a6baeef429406eafd2bfc2f341ef5dbfa7ad
willemstoker <willem.stoker@gmail.com>
2019-05-02 21:18:52 +0200

    Create apstopvoutput.py

---
+++ apstopvoutput.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+import requests
+import json
+from datetime import date
+from datetime import datetime
+
+#id's and keys
+ECU_ID = '<ecuid>'
+PV_OUTPUT_SYSTEMID = '<systemid>'
+PV_OUTPUT_APIKEY = '<apikey>'
+
+#create a file somewhere and put the current datetime in it in format "20190502 08:00"
+#enter the path and filename below
+LAST_UPDATE_FILE = "/home/pi/aps/lastupdate"
+
+#usually all below this point should not be modified
+MAX_NUMBER_HISTORY = 6
+APSYSTEMS_URL = 'http://api.apsystemsema.com:8073/apsema/v1/ecu/getPowerInfo'
+PVOUTPUT_URL = 'http://pvoutput.org/service/r2/addstatus.jsp'
+
+def readLastUpdate():
+    f = open(LAST_UPDATE_FILE,"r")
+    datestring = f.read()
+    f.close()
+    return datetime.strptime(datestring, "%Y%m%d %H:%M")
+
+def writeLastUpdate(timestringminutes):
+    f = open(LAST_UPDATE_FILE,"w+")
+    f.write(getDateStringOfToday()+ ' ' +timestringminutes)
+    f.close()
+
+def getDateStringOfToday():
+    return date.today().strftime("%Y%m%d");
+
+def getDataFromAPS():
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    data = {
+      'ecuId': ECU_ID,
+      'filter': 'power',
+      'date': getDateStringOfToday()
+    }
+
+    response = requests.post(APSYSTEMS_URL, headers=headers, data=data)
+    return response.json();
+
+def sendUpdateToPVOutput(timestringminutes, powerstring):
+    pvoutputdata = {
+      'd': getDateStringOfToday(),
+      't': timestringminutes,
+      'v2': powerstring
+    }
+
+    headerspv = {
+        'X-Pvoutput-SystemId': PV_OUTPUT_SYSTEMID,
+        'X-Pvoutput-Apikey': PV_OUTPUT_APIKEY
+    }
+
+    responsepv = requests.post(PVOUTPUT_URL, headers=headerspv, data=pvoutputdata)
+
+    print ("Response: " + responsepv.text + " updated: " + timestringminutes + " power: " + powerstring)
+
+rootdict = getDataFromAPS()
+timesstring = rootdict.get("data").get("time")
+powersstring = rootdict.get("data").get("power")
+
+timelist = json.loads(timesstring)
+powerlist = json.loads(powersstring)
+latestUpdate = readLastUpdate()
+print("Found latest update: ")
+print(latestUpdate)
+
+i = len(timelist) - 1
+count = 0;
+while i >= 0 and count < MAX_NUMBER_HISTORY:
+    timestringminutes = timelist[i][:-3] #get time and strip the seconds
+    powerstring = powerlist[i] #get power
+
+    currentUpdate = datetime.strptime(getDateStringOfToday()+ ' ' +timestringminutes, "%Y%m%d %H:%M")
+
+    if currentUpdate > latestUpdate:
+        sendUpdateToPVOutput(timestringminutes, powerstring)
+    else:
+        print("No update needed for: " + timestringminutes)
+
+    if count == 0:
+        writeLastUpdate(timestringminutes)
+        
+    i -= 1
+    count += 1
